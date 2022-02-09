ac678dab0414631fbfee6b66ae2868d062f113b2
arminZolfaghari <arminzd1@gmail.com>
2022-02-09 14:22:30 +0330

    add typeorm and add user entity

---
+++ src/database/entities/User.ts
@@ -0,0 +1,20 @@
+import {Entity, PrimaryGeneratedColumn, Column, BaseEntity} from 'typeorm'
+
+@Entity()
+export class User extends BaseEntity {
+    @PrimaryGeneratedColumn()
+    id: number;
+
+    @Column()
+    firstName: string;
+
+    @Column()
+    lastName: string;
+
+    @Column()
+    password: string;
+
+    @Column()
+    username: string;
+}
+
